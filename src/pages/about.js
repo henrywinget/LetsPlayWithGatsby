ae2f3b06e9ed1eb7f529e77bdefd05d24945bd0e
ninja2 <henrywinget@yahoo.com>
2019-04-25 20:26:42 -0500

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "gatsby";
+import Header from '../components/header';
+
+export default () => (
+    <div style={{ color: 'teal' }}>
+        <Header headerText="I'm a header bish!" />
+        <p>This is an about page. Nothing to see here.</p>
+    </div>
+)
